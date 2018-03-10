7497cb65a3a9203a64c072037d2c02dcc6fea5b3
mariana1hdzc <mariana1hdzc@gmail.com>
2018-03-10 11:49:20 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import {CardPanel, Row, Col,Input,Button,Card} from 'react-materialize';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import './App.css';
 import Post from'./Post/component/Post';
 
@@ -14,15 +16,49 @@ class App extends Component{
       newPostBody:'',
       newPostUser:''
     }
+    var config = {
+      apiKey: "AIzaSyCYTMkLrNxVEcl80fTYM2pmqHwpKZVHImE",
+      authDomain: "react-chat-2799d.firebaseapp.com",
+      databaseURL: "https://react-chat-2799d.firebaseio.com",
+      projectId: "react-chat-2799d",
+      storageBucket: "",
+      messagingSenderId: "818941127099"
+    };
+    //referencia al objeto de firebase
+    this.app = firebase.initializeApp(config);
+    //referencia a la base de datos
+    this.database = this.app.database();
+    //referencia a la "tabla" post
+    this.databseRef = this.database.ref().child('post');
   }
+
+  componentWillMount(){
+    this.databseRef.on('child_added', snapshot =>{
+      const response = snapshot.val();
+      this.updateLocalState(response);
+  })}
+
+  updateLocalState = (response) => {
+    //copia del estado actual
+    const posts = this.state.posts;
+    //actualiza la copia del estado
+    posts.push(response);
+    //actualizar el estado
+    this.setState(posts);
+  }
+
   //Esta función concatena los valores y los envía al array
-  addPost=()=>{
+  addPost = (newPostBody, newPostUser) => {
     const newState = Object.assign({},this.state);
 
-    newState.posts.push({name: this.state.newPostUser, message: this.state.newPostBody});
+    //newState.posts.push({name: this.state.newPostUser, message: this.state.newPostBody});
+    //copiar el estado
+    const postToSave = {name: this.state.newPostUser, message: this.state.newPostBody}
 
-    newState.newPostBody= '';
-    this.setState(newState);
+    //newState.newPostBody= '';
+    //this.setState(newState);
+    //guarda en firebase nuestros posts
+    this.databseRef.push().set(postToSave);
   }
   //Esta función gestiona el valor del input mensaje
   handlePostEditorInputChange = (e)=>{
